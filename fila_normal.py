568fb4b271f98a7b7d2f71d507e13cd7d4c8f404
Gilmar <gilmar.jesus@gmail.com>
2022-01-31 22:49:00 -0300

    Criado a classe fila normal.
    testado o codigo criado.

--- fila_normal.py
+++ fila_normal.py
@@ -1,8 +1,24 @@
 class Filanormal:
-    codigo:int = 0
+    codigo: int = 0
     fila = []
     clientesatendidos = []
-    senhatual:str = ''
+    senhatual: str = ''
 
-    def gerasenhaatual(self)-> None:
+    def gerasenhaatual(self) -> None:
         self.senhatual = f'NM{self.codigo}'
+
+    def resetafila(self):
+        if self.codigo >= 100:
+            self.codigo = 0
+        else:
+            self.codigo += 1
+
+    def atualizafila(self) -> None:
+        self.resetafila()
+        self.gerasenhaatual()
+        self.fila.append(self.senhatual)
+
+    def chamacliente(self, caixa: int) -> str:
+        cliente_atual: str = self.fila.pop(0)
+        self.clientesatendidos.append(cliente_atual)
+        return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
