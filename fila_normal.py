059a62373e0c042a0ceb204859cc6751dd1f1cdf
Gilmar <gilmar.jesus@gmail.com>
2022-02-01 12:38:42 -0300

    Criado a fila_base com metodos e atributos comuns entre classes.

--- fila_normal.py
+++ fila_normal.py
@@ -1,24 +1,18 @@
-class Filanormal:
-    codigo: int = 0
-    fila = []
-    clientesatendidos = []
-    senhatual: str = ''
 
+
+from fila_base import FilaBase
+
+
+class Filanormal(FilaBase):
     def gerasenhaatual(self) -> None:
-        self.senhatual = f'NM{self.codigo}'
-
-    def resetafila(self):
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
+        self.senha_atualatual = f'NM{self.codigo}'
 
     def atualizafila(self) -> None:
-        self.resetafila()
+        self.reseta_fila()
         self.gerasenhaatual()
-        self.fila.append(self.senhatual)
+        self.fila.append(self.senha_atual)
 
     def chamacliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
-        self.clientesatendidos.append(cliente_atual)
+        self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
