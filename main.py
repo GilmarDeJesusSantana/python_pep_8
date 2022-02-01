8fa814598f75f8c68f046d7d2e7d5956ede83fec
Gilmar <gilmar.jesus@gmail.com>
2022-01-31 23:18:45 -0300

    Implementando codigo respeitando a PEP8.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fila_normal import Filanormal
+from fila_prioritaria import FilaPrioritaria
 
-fila_teste = Filanormal()
-fila_teste.atualizafila()
-fila_teste.atualizafila()
-fila_teste.atualizafila()
-fila_teste.atualizafila()
-print(fila_teste.chamacliente(5))
-print(fila_teste.chamacliente(10))
+# fila_teste = Filanormal()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# print(fila_teste.chamacliente(5))
+# print(fila_teste.chamacliente(10))
+
+fila_teste2 = FilaPrioritaria()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+print(fila_teste2.chama_cliente(10))
+print(fila_teste2.chama_cliente(1))
+print(fila_teste2.estatistica('10/01/1993', 198, 'detail' ))
